2c79458411111c6ce6b6078cbb31ad1bff7121d2
Brandon Fulk <bf@the42.com>
2013-04-10 17:33:03 -0500

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mimir::Application.config.secret_token = '3882fc49e8f9c83ba73b36def256a3eb4e39c446f49c92943e126ef52ad82959f6c349aacf6e2ffc58e3c6e36804252d62b49051169d2df613264dffb2e229b1'
